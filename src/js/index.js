9da754b3429eeb39d9bf37adb6a683b931e37580
aristarkhov kirill <aristarkhov.kiril@mail.ru>
2023-06-08 18:57:29 +0700

    fix: update 1.3

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,32 @@ import imgCheck from '/images/check.svg'
 import imgPerson from '/images/person.svg'
 import imgPhone from '/images/phone.svg'
 import imgRepair from '/images/repair.svg'
+import imgArrowsDown from '/images/arrows-down.svg'
 import imgTopscreen from '/images/top-screen-image.jpg'
-import imgArrowsdown from '/images/arrows-down.svg'
+/* import lenovoImg from '/images/lenovo.png'
+import samsungImg from '/images/samsung.png'
+import appleImg from '/images/apple.png'
+import viewsonicImg from '/images/viewsonic.png'
+import boschImg from '/images/bosch.png'
+import hpImg from '/images/hp.png'
+import acerImg from '/images/acer.png'
+import sonyImg from '/images/sony.svg'
+import arrowbtnImg from '/images/arrow-btn.svg'
+import gradient from '/images/gradient.png'
+*/
+/* import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.brands__slider', {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  pagination: {
+    el: '.brands__pagination',
+    bulletClass: 'brands__pagination-bullet',
+    bulletActiveClass: 'brands__pagination-bullet--active',
+    clickable: true,
+  },
+}); */
+
+
